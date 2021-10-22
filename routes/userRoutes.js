bd3d3bcaa93e992dd9ac15ef04b1e1cabf1cfffd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-10-22 13:24:35 -0500

    [+] Routes /refreshToken: Actualizacion silenciosa para obtener nuevo Token de Actualizacion

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const
     express = require( 'express' ),
     router = express .Router(),
     passport = require( 'passport' ),
+    jwt = require( 'jsonwebtoken' ),
     User = require( '../models/user' ),
     { getToken, COOKIE_OPTIONS, getRefreshToken } = require( '../authenticate' );
 
@@ -91,6 +92,85 @@ router .post( '/login', passport .authenticate( 'local' ), ( request, response, 
 
                 });
             }, error => next( error ) );
+
+});
+
+router .post( '/refreshToken', ( request, response, next ) => {
+
+    const
+        { signedCookies = {} } = request,
+        { refreshToken } = signedCookies;       // ? Recuperamos el token de actualización de las cookies firmadas.
+
+    console .log( request.signedCookies );
+
+    // ? Verifica si el Token de Actualizacion fue pasado en el Request (Peticion).
+    if( refreshToken ) {
+
+        try {
+            const
+                payload = jwt .verify( refreshToken, process .env .REFRESH_TOKEN_SECRET ),      // ? Verifica Token de Actualización con el SECRET usado para generarlo
+                userId = payload ._id;                                                          // ? Extrae la carga útil del Token (ID de usuario)
+
+            // * Consulta si existe un usuario registrado con el ID obtenido del Token de Actualizacion enviado en la Peticion
+            User .findOne({ _id: userId })
+                    .then( user => {
+
+                        // ? Verifica si se obtuvo un usuario de la base de datos.
+                        if( user ) {
+                            // * Busca el token de actualización contra el registro de usuario en la base de datos.
+                            // ! NOTA: En caso de cierre de sesión de todos los dispositivos, se eliminarán todos los Tokens de Actualización del usuario.
+                            const tokenIndex = user .refreshToken .findIndex( item => item .refreshToken === refreshToken );
+
+                            // ? Verifica si hay un indice NO valido en la busqueda (En otras palabras que no encontró el Token de Actualización)
+                            if( tokenIndex === -1 ) {
+                                response .statusCode = 401;
+                                response .send( 'Unauthorized. User without refresh token.' );
+                            }
+                            else {
+                                // * Encuentra el Token de Actualización actualiza los Tokens
+                                const
+                                    token = getToken({ _id: userId }),                      // ? Obtiene nuevo Token de Autenticacion
+                                    newRefreshToken = getRefreshToken({ _id: userId });     // ? Obtiene nuevo Token de Actualización
+
+                                user .refreshToken[ tokenIndex ] = { refreshToken: newRefreshToken };   //  ? Si el token de actualización existe, cree uno nuevo y reemplácelo.
+
+                                user .save( ( err, theUser ) => {
+
+                                    // ! Verifica si hay un error al guardar cambios en la BD
+                                    if( err ) {
+                                        response .statusCode = 500;
+                                        response .send( err );
+                                    }
+                                    else {
+                                        // * Establece el Token de Actualizacion como Cookie y envia respuesta.
+                                        response .cookie( 'refreshToken', newRefreshToken, COOKIE_OPTIONS );
+                                        response .send({ success: true, user: theUser, token });
+                                    }
+
+                                });
+
+                            }
+
+                        }
+                        else {
+                            // ? Si el usuario no esta registrado
+                            response .statusCode = 401;
+                            response .send( 'Unauthorized, Unregistered user.' );
+                        }
+
+                    }, error => next( error ) );
+
+        } catch (error) {
+            response .statusCode = 401;
+            response .send( 'Unauthorized, An error occurred.' );
+        }
+
+    }
+    else {
+        // ! Si NO existe el Token de Actualizacion
+        response .statusCode = 401;
+        response .send( 'Unauthorized. Request without refresh token.' );
+    }
 
 });
 
